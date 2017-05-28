dee11de5589311ed98d356f7cb7c012d5be14683
aaron <aaron@aaronde-MacBook-Air.local>
2017-05-28 22:01:57 +0800

    change file name

--- src/components/Home/DayFilter.js
+++ src/components/Home/DayFilter.js
@@ -2,18 +2,11 @@ import React from 'react'
 import DayButtons from './DayButtons'
 import DayGoTo from './DayGoTo'
 class Days extends React.Component {
-    constructor() {
-        super()
-        this.onSelectDay = this.onSelectDay.bind(this)
-    }
-    onSelectDay(day) {
-        this.props.onChangeDay(day)
-    }
     render() {
         let { name, grade, type } = this.props
         return (<div>
-            <DayButtons onSelectDay={this.onSelectDay} />
-            <DayGoTo onSelectDay={this.onSelectDay} />
+            <DayButtons onSelectDay={this.changeDay} />
+            <DayGoTo onSelectDay={this.changeDay} />
         </div>)
     }
 }
